18ae2f590ca88c33a71cfe0240f32b79dedeffab
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-20 09:53:37 +0900

    231120 1교시 파이어 인증 뷰 간단히 구성하기

--- app/src/main/java/com/example/firebasetest/lhs/AuthActivity.kt
+++ app/src/main/java/com/example/firebasetest/lhs/AuthActivity.kt
@@ -1,16 +1,31 @@
 package com.example.firebasetest.lhs
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firebasetest.lhs.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    lateinit var binding : ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // 다음 시간에, 이메일, 구글 인증, 후
         // 인증 후 2번째 페이지 접근 확인하고,
         // 그리고 나서, 2번째 페이지 부분을 아래의 기능으로 구성.
         // 스토어, 스토리지에 접근 및 이용하는 부분을 보여주기.
+
+
+        // MyApplication 인증 함수를 이용해서, 인증이 되면, 특정 뷰로 인증됨을 확인
+        if(MyApplication.checkAuth()){
+            Log.d("lhs", "로그인 인증이 됨")
+        } else {
+            Log.d("lhs", "로그인 인증이 안됨")
+        }
+
+
+        // omCreate
     }
 }
--- app/src/main/java/com/example/firebasetest/lhs/MyApplication.kt
+++ app/src/main/java/com/example/firebasetest/lhs/MyApplication.kt
@@ -9,11 +9,22 @@ import com.google.firebase.ktx.Firebase
 class MyApplication : MultiDexApplication(){
 
     companion object {
+        // 원래는 자바에서, 해당 클래스의 멤버를 사용하는 방법 2가지.
+        // 1) 인스턴스를 생성해서 접근 2) static으로 클래스명으로 접근.
+        // 예) 자바: A a = new A(); -> a. 접근.
+        // 예) 코틀린 : val a = A(); -> a. 접근.
         // static과 비슷, 해당 클래스명으로 멤버에 접근이 가능함.
         // 인증 기능에 접근하는 인스턴스가 필요함.
+        // 선언만 하고, 초기화를 안했음.
+        // 초기화를 밑에 onCreate
         lateinit var auth: FirebaseAuth
         // 인증할 이메일
         var email : String? = null
+
+        // MyApplication.checkAuth() : 이렇게 클래스명.함수 및 특정 변수에 접근이 가능함(companion object 이것을 사용해서 가능)
+        // 즉, 인스턴스를 생성하지 않아도 바로 MyApplication의 함수 및 특정 변수를 사용가능(자동으로 메모리에 생성되어 있다는 개념)
+        // 이것을 사용하지않으면 인스턴스를 생성 후 해당 클래스 접근이 가능
+        // 그렇기 때문에, MyApplication은 동작을 하면 바로 초기화가 되어 메모리상에 둥둥 떠 다님 -> 그래서, 바로 해당 클래스에 접근하여 사용 가능
 
         // 해당 로직은 auth 자체의 로직(개발자가 임의로 설정한 게 아님)
         fun checkAuth():Boolean {
@@ -35,6 +46,7 @@ class MyApplication : MultiDexApplication(){
     // 생명주기, 최초 1회 동작을 함.
     override fun onCreate() {
         super.onCreate()
+        // 초기화를 함.
         auth = Firebase.auth
     }
 }
