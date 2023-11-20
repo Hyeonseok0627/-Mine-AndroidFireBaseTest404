e4f50883ab1d747cfa9b4a5cb4d6496d81d7ef96
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-20 10:54:20 +0900

    231120 2교시 파이어 인증 매니페스트에 만든 후처리 함수 작업중.

--- app/src/main/java/com/example/firebasetest/lhs/AuthActivity.kt
+++ app/src/main/java/com/example/firebasetest/lhs/AuthActivity.kt
@@ -3,8 +3,12 @@ package com.example.firebasetest.lhs
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebasetest.lhs.databinding.ActivityAuthBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
 
 class AuthActivity : AppCompatActivity() {
     lateinit var binding : ActivityAuthBinding
@@ -29,50 +33,86 @@ class AuthActivity : AppCompatActivity() {
             // 인증이 되면, mode에 따라 보여주는 함수를 동작시키기
             changeVisi("logout")
         }
+        // 구글 로그인 기능 확인.
+        // 1) 구글 인증 버튼 눌러서, 2) 후처리 함수를 호출하기.
+        // 1) 구글 인증 버튼 -> 구글의 파이어베이스 서버에 접속하고, 관련 인증을 가지고 돌아오기.
+        // 2) 후처리 함수 만들기. 구글의 계정의 정보를 가지고와서, 처리하는 로직.
+        val requestLauncher = registerForActivityResult(
+            // 후처리 하는 함수가 정해져 있는데, 이 함수를 인증, 권한 확인용
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            // 실제 작업은 여기에 이루어짐.
+            // 구글 인증 결과 처리.
+            // it.data: 이 부분이 구글로부터 받아온 계정 정보.
+            // 구글의 계정정보를 받아오는 인스턴스의 함수(GoogleSignIn.getSignedInAccountFromIntent(it.data))이며, 이걸 task에 담는다.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            // 로그인 정보가 있는지? 없는지? 또는 네트워크 연결 오류 등으로
+            // 정보가 받거나 못 받거나 할 가능성이 있으면, 무조건, try- catch 구문 사용함.
+            try {
+                // 계정 정보 가져오기.
+                val account = task.getResult(ApiException::class.java)
+                // 계정의 정보 가져오기.
+                val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+                // 우리가 만든 MyApplication에서 auth로 확인
+                MyApplication.auth.signInWithCredential(credential)
+                    // 정보를 잘 가지고 왔을 때 , 수행이 될 콜백함수,
+                    .addOnCompleteListener(this) {
+                        // 수행할 작업.
+                            task ->
+                        if(task.isSuccessful){
+                            MyApplication.email = account.email
+                            changeVisi("login")
+                        } else {
+                            changeVisi("logout")
+                        }
+                    }
+            } catch (e:ApiException){
+                changeVisi("logout")
+            }
+        }
 
-
-        // omCreate
+        // onCreate
     }
 
-    // 임의의 함수를 만들기.
+    //임의의 함수를 만들기.
     // 인증의 상태에 따라서, 로그인 화면을 표시 여부.
     // 예) 로그인이 되면, 로그아웃 버튼을 보이고,
     // 예) 로그인이 안되면, 로그아웃 버튼을 사라지게 만들기. 등
-
     fun changeVisi(mode: String) {
-        if (mode == "login") {
+        if (mode === "login") {
             // 로그인이 되었다면, 인증된 이메일도 이미 등록이 되어서, 가져와서 사용하기.
             binding.authMainText.text = "${MyApplication.email} 님 반가워요."
             binding.logoutBtn.visibility = View.VISIBLE
             // 그외 버튼, 에디트 텍스트뷰, 회원가입, 구글인증 다 안보이게 설정.
             binding.signInBtn.visibility = View.GONE
-            binding.signInBtn2.visibility = View.GONE
             binding.googleAuthInBtn.visibility = View.GONE
             binding.signInBtn2.visibility = View.GONE
             binding.authEmailEdit.visibility = View.GONE
             binding.authPasswordEdit.visibility = View.GONE
             binding.logInBtn.visibility = View.GONE
-        }else if( mode === "logout") {
-            binding.authMainText.text = "로그인 하거나 회원가입 해주세요."
+        } else if( mode === "logout") {
+            binding.authMainText.text ="로그인 하거나 회원가입 해주세요."
             binding.logoutBtn.visibility = View.GONE
             // 그외 버튼, 에디트 텍스트뷰, 회원가입, 구글인증 다 안보이게 설정.
             binding.signInBtn.visibility = View.VISIBLE
             binding.signInBtn2.visibility = View.VISIBLE
             binding.googleAuthInBtn.visibility = View.VISIBLE
-            binding.signInBtn2.visibility = View.VISIBLE
             binding.authEmailEdit.visibility = View.VISIBLE
             binding.authPasswordEdit.visibility = View.VISIBLE
             binding.logInBtn.visibility = View.VISIBLE
-        }else if( mode === "signIn") {
+
+        } else if( mode === "signIn") {
             binding.logoutBtn.visibility = View.GONE
             // 그외 버튼, 에디트 텍스트뷰, 회원가입, 구글인증 다 안보이게 설정.
             binding.signInBtn.visibility = View.GONE
             binding.signInBtn2.visibility = View.GONE
             binding.googleAuthInBtn.visibility = View.GONE
-            binding.signInBtn2.visibility = View.GONE
             binding.authEmailEdit.visibility = View.VISIBLE
             binding.authPasswordEdit.visibility = View.VISIBLE
             binding.logInBtn.visibility = View.GONE
+
         }
     }
+
+
 }
