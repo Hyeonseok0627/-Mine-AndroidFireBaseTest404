183e26a51c2d6ca8ea18b83982b673eca81abf03
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-21 09:51:16 +0900

    231121 1교시 이미지 공유 앱, 기본, 뼈대, 리사이클러 작업중4, 어댑터 구성중

--- app/src/main/java/com/example/firebasetest/lhs/imageShareApp/recycler/MyAdapter.kt
+++ app/src/main/java/com/example/firebasetest/lhs/imageShareApp/recycler/MyAdapter.kt
@@ -1,9 +1,40 @@
 package com.example.firebasetest.lhs.imageShareApp.recycler
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firebasetest.lhs.databinding.ItemMainBinding
+import com.example.firebasetest.lhs.imageShareApp.model.ItemData
+
 // 뷰홀더 설정.
-//
-class  MyViewHolder(val)
-class MyAdapter {
+// 아이템 요소의 뷰를 설정하기. -> 목록요소 뷰 -> 메뉴 아이템 뷰 구성. 백
+
+class  MyViewHolder(val binding : ItemMainBinding) : RecyclerView.ViewHolder(binding.root)
+class MyAdapter (val context: Context, val itemList: MutableList<ItemData>)
+    : RecyclerView.Adapter<MyViewHolder>() {
     // 목록 요소에, 데이터를 연결시켜주는 역할
     // 뷰홀더도 있음.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return MyViewHolder(ItemMainBinding.inflate(layoutInflater))
+    }
+
+    // 넘어온 전체 데이터 크기
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    // 뷰와 데이터 붙이기 작업(연동)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = itemList.get(position)
+
+        holder.binding.run {
+            emailResultView.text = data.email
+            dateResultView.text = data.date
+            contentResultView.text = data.content
+        }
+
+        // 스토리지에서 이미지 불러와서, Glide로 출력하기
+    }
 }
