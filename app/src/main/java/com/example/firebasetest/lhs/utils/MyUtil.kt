f33f33a1766d8c990267dfa1c856fa0dd12eb6c6
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-21 10:52:26 +0900

    231121 2교시 이미지 공유 앱, 글쓰기 화면 및 기능 구현 중.

--- app/src/main/java/com/example/firebasetest/lhs/utils/MyUtil.kt
+++ app/src/main/java/com/example/firebasetest/lhs/utils/MyUtil.kt
@@ -1,38 +1,36 @@
-package com.example.firebasetest.lhs.utils
+package com.example.firebasetest.lsy.Utils
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.example.firebasetest.lhs.Manifest
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class MyUtil {
-    // 자주 쓰는 기능들을 모아뒀다가, 필요 시 사용하기.
-    // 1) 날짜 변환부터 여기에 사용.
+    // 자주 쓰는 기능들을 모아뒀다가, 필요시 사용하기.
+    // 1) 날짜 변환 부터 여기에 사용.
     // 2) 퍼미션 권한 물어보기.
     companion object {
 
-
-        // 퍼미션 체크 함수 만들기.
-        // activity로 매개변수를 선택(인증 처리에서 이것을 잘 사용함)
+        //퍼미션 체크 함수 만들기.
         fun checkPermission (activity: AppCompatActivity) {
-            // 후처리 함수, 1번 앱 -> 2번 앱으로 가서,
-            // 1) 사진 선택, 2) 권한을 획득을 하건, 뭔가 2번 앱에서 가져오기.
-            // 2번에 갔다가, 돌아 왔을때 처리하는 함수
+            //후처리 함수, 1번 앱 -> 2번 앱으로 가서,
+            // 1)사진 선택, 2) 권한을 획득을 하건, 뭔가 2번 앱에서 가져오기.
+            // 2번에 갔다가,돌아 왔을 때 처리하는 함수
             // 1번, 해당 권한을 요청하는 인텐트 요청.
             // 2번, 처리하는 함수 구성.
             val requestPermissionLauncher = activity.registerForActivityResult(
-                // 권한 요청 후 확인시 사용
-                // 샘플코드들임.
+                // 권한 요청 후, 확인시 사용.
+                // 샘플코드임.
                 ActivityResultContracts.RequestPermission()
             ) {
-                if(it) {
-                    Toast.makeText(activity, "권한 승인됨", Toast.LENGTH_SHORT).show()
-                }else {
-                    Toast.makeText(activity, "권한 승인안됨", Toast.LENGTH_SHORT).show()
+                if (it) {
+                    Toast.makeText(activity,"권한 승인됨",Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(activity,"권한 승인 안됨",Toast.LENGTH_SHORT).show()
                 }
             }
 
